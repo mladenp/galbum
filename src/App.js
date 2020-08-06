a475eb167e599d5bc015cfd5b7d9c5fe56328021
Mladen Petrovic <mladenpetrovic88@gmail.com>
2020-08-06 12:02:07 +0200

    gapi lib added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+
+const API_KEY = "AIzaSyCBbdr3PXFQ0_-fQve7hLwh5_dbPWyzva8"
+const CLIENT_ID = "662849664251-0nko7svri1f0uf42urrjjuf37kapr1ml.apps.googleusercontent.com"
+const SCOPE = "https://www.googleapis.com/auth/photoslibrary.readonly"
+let gapi = window.gapi;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function start() {
+    // 2. Initialize the JavaScript client library.
+    gapi.client.init({
+      'apiKey': API_KEY,
+      'clientId': CLIENT_ID,
+      'scope': SCOPE,
+    }).then(function () {
+      // return gapi.client.request({
+      //   'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+      // })
+      console.log('signin')
+      gapi.auth2.getAuthInstance().signIn()
+    }).then(function (response) {
+      console.log(response.result);
+    }, function (err) {
+      console.log('Error: ', err);
+    });
+  };
+
+  gapi.load('client', start);
+  return <></>
 }
 
-export default App;
+export default App
